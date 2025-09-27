19fa2539f509f2dc638ffd2a8709537937ffae2c
Alejandro Arevalo Covas <alejandroarevalocovas@gmail.com>
2025-09-27 12:04:25 +0200

    fixed bug with token

--- backend_FastApi/main.py
+++ backend_FastApi/main.py
@@ -28,7 +28,7 @@ import os
 DATABASE_URL = os.environ.get("DATABASE_URL")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = os.environ.get("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
